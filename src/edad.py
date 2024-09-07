45d6a0ac368548783eb0f5e5e981a16fc845b0ea
ValbuenaTKevin <159049248+ValbuenaTKevin@users.noreply.github.com>
2024-09-07 14:09:19 -0500

    Update edad.py

--- src/edad.py
+++ src/edad.py
@@ -1,15 +1,18 @@
+from time import localtime
 def evaluar(dia, mes, anno):
-    # TODO: Coloca aquí el código del ejercicio 6: Edad
-    return "";
-
+    hoy = localtime()
+    edad = hoy.tm_year - anno
+    if (hoy.tm_mon, hoy.tm_mday) < (mes, dia):
+        edad -= 1
+    return f"Usted tiene {edad} años"
 if __name__ == '__main__':
     print("Ingrese su fecha de nacimiento")
-    print("Día:", end="")
+    print("Día: ", end="")
     dia = int(input())
-    print("Mes:", end="")
+    print("Mes: ", end="")
     mes = int(input())
-    print("Año:", end="")
+    print("Año: ", end="")
     anno = int(input())
-        
+    
     respuesta = evaluar(dia, mes, anno)
     print(respuesta)
