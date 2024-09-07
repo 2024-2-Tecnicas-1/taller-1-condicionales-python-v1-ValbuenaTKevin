0219652357e9ffef91cddaa9253514075905d189
ValbuenaTKevin <159049248+ValbuenaTKevin@users.noreply.github.com>
2024-09-07 14:23:08 -0500

    Update triangulo.py

--- src/triangulo.py
+++ src/triangulo.py
@@ -1,7 +1,12 @@
 def evaluar(a, b, c):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
-
+    if a + b <= c or a + c <= b or b + c <= a:
+        return "No es un triángulo válido"
+    if a == b == c:
+        return "El triángulo es equilátero"
+    elif a == b or a == c or b == c:
+        return "El triángulo es isósceles"
+    else:
+        return "El triángulo es escaleno"
 if __name__ == '__main__':
     print("a:", end="")
     a = float(input())
