7b7f528f6c053e548ea8782509ad424abd42d7d5
ValbuenaTKevin <159049248+ValbuenaTKevin@users.noreply.github.com>
2024-09-07 14:15:45 -0500

    Update letra_o_numero.py

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,10 +1,18 @@
 def evaluar(caracter):
-    # TODO: Coloca aquí el código del ejercicio 4: Letra o número
-    return "";
+    if caracter.isdigit():
+        return "Es número"
+
+    elif caracter.isalpha():
+        if caracter.isupper():
+            return "Es letra mayúscula"
+        elif caracter.islower():
+            return "Es letra minúscula"
+    
+    return "No es letra ni número"
 
 if __name__ == '__main__':
     print("Caracter:", end='')
-    caracter = input()
+    caracter = input().strip()
         
     respuesta = evaluar(caracter)
     print(respuesta)
