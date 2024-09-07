0fccf0c84be8dc94a4eaf943877f9bb00bd34fee
ValbuenaTKevin <159049248+ValbuenaTKevin@users.noreply.github.com>
2024-09-07 14:13:29 -0500

    Update imc.py

--- src/imc.py
+++ src/imc.py
@@ -1,13 +1,21 @@
 def evaluar(peso, estatura, edad):
-    # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-    return "";
-
+    imc = peso / (estatura ** 2)
+    if edad < 45:
+        if imc < 22:
+            return "bajo"
+        else:
+            return "medio"
+    else:
+        if imc < 22:
+            return "medio"
+        else:
+            return "alto"
 if __name__ == '__main__':
-    print("Peso:", end="")
-    peso = int(input())
-    print("Estatura:", end="")
+    print("Peso:", end=" ")
+    peso = float(input())
+    print("Estatura:", end=" ")
     estatura = float(input())
-    print("Edad:", end="")
+    print("Edad:", end=" ")
     edad = int(input())
         
     respuesta = evaluar(peso, estatura, edad)
