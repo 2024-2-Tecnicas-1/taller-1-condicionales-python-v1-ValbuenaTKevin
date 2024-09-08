14e33af13cd381091e2501d4c23577fe4947d117
Kevin <valbuenatkevin@gmai.com>
2024-09-07 21:50:46 -0500

    Correciones
    Pa'l 5 :D

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,25 +1,19 @@
 def evaluar(num_victorias_a, num_victorias_b):
-    if num_victorias_a < 0 or num_victorias_b < 0:
-        return "Inválido"
-    if num_victorias_a > 7 or num_victorias_b > 7:
-        return "Inválido"
-    if num_victorias_a >= 6 and num_victorias_a >= num_victorias_b + 2:
+     if (num_victorias_a == 7 and (num_victorias_b == 5 or num_victorias_b == 6)):
         return "Ganó A"
-    if num_victorias_b >= 6 and num_victorias_b >= num_victorias_a + 2:
-        return "Ganó B"
-    
-    if num_victorias_a == 5 and num_victorias_b == 7:
-        return "Ganó B"
-    if num_victorias_b == 5 and num_victorias_a == 7:
-        return "Ganó A"  
-    if num_victorias_a == 6 and num_victorias_b == 7:
-        return "Ganó B"
-    if num_victorias_b == 6 and num_victorias_a == 7:
-        return "Ganó A"
-    if (num_victorias_a >= 0 and num_victorias_a < 6) or (num_victorias_b >= 0 and num_victorias_b < 6):
+     elif (num_victorias_b == 7 and (num_victorias_a == 5 or num_victorias_a == 6)):
+        return "Ganó B";
+     elif num_victorias_a == 6 and num_victorias_b <= 4:
+        return "Ganó A";
+     elif num_victorias_b == 6 and num_victorias_a <= 4:
+        return "Ganó B";
+     elif num_victorias_a <= 6 and num_victorias_b <= 6:
         return "Aún no termina"
-    
-    return "Inválido"
+     elif (num_victorias_a > 7 or num_victorias_b > 7 or (num_victorias_a == 7 and num_victorias_b < 5)):
+        return "Inválido"
+     else: 
+        return "Inválido"
+
 
 if __name__ == '__main__':
     print("Juegos ganados por A:", end=" ")
